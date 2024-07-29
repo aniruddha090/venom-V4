9c3b879c5042ffa6c0c86ff28f023139215e0cf2
aniruddha090 <78275932+aniruddha090@users.noreply.github.com>
2024-07-29 19:31:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7399735507:AAHrbAgM3NLApic89e57d1C87U-0876Sg-8")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("sam1408_bot")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6092284993")
 
 
 
